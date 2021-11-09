953f2e8325aacc8e19180f26879335a9ebe5a77e
LelayJ-ESIEE <juleslelay@edu.esiee.fr>
2021-11-09 09:38:11 +0100

    Commit last version (already sent)

--- Sciences_Ingenieur/SI1-Algebre_avance/TP3/src.py
+++ Sciences_Ingenieur/SI1-Algebre_avance/TP3/src.py
@@ -12,7 +12,7 @@ f = lambda x : 1/(1+8*x**2)
 # Parameters to generate n points from f(x) for x in [a;b]
 a = -5
 b = 5
-n = 71
+n = 11
 
 ##########
 # Script #
@@ -34,9 +34,24 @@ def Newton(X, k, x):
 # Evaluate divided diff
 def diff_divise(X, Y):
     yc = np.copy(Y)
+    l = [ [x] for x in X]
+
+    for i in range(n):
+        l[i].append(round(yc[i],4))
+
     for ref in range(n-1):
+        l[ref] += ["-"]*(n-ref-1)
         for k in range(ref+1, n):
             yc[k] = (yc[k]-yc[ref])/(X[k]-X[ref])
+            l[k].append(round(yc[k],4))
+    l = [["xk","yk"] + [" "]*n] + [[":-:"]*(n+2)] + l
+    s=""
+    for i in range(n+2):
+        s +="|"
+        for j in range(n+1):
+            s+= str(l[i][j]) + "|"
+        s += "\n"
+    print(s)
     return yc
 
 
