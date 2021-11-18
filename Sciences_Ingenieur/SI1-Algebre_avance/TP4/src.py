f4aa2cd522a2da5d13c11cf0001a9cc13805b575
LelayJ-ESIEE <jules.lelay@edu.esiee.fr>
2021-11-18 09:54:10 +0100

    Implement complex draw

--- Sciences_Ingenieur/SI1-Algebre_avance/TP4/src.py
+++ Sciences_Ingenieur/SI1-Algebre_avance/TP4/src.py
@@ -1,28 +1,7 @@
 import math
 import numpy as np
-from matplotlib import colors, pyplot as plt
+from matplotlib import pyplot as plt
 from matplotlib import collections as mc
-
-##########
-#Entries #
-##########
-
-# list of points
-L = [(0,0), (1,1), (2,0)] # Conic
-# L = [(0, 2), (1, 2), (1, 0), (1, 0), (0, 0), (-1, 0), (-1, 0), (-1, 2), (0, 2)] # Egg
-# L = [(0,0), (1,10), (3,-10), (4,0)] # Sin-like
-# L = [(1,1), (0.5,0), (0,0), (0,1), (0.5,1), (1,0)] # Alpha
-# L = [(-0.5,1), (1,0.5), (0,0), (-1,0.5), (0.5,1)] # Gamma
-# L = [(0,2), (1,3), (2,2), (0,0)] # Half heart
-# L = [(0,0), (-0.3, 0), (-0.3, 0.3)] # Quarter of circle
-# L = [(0,0), (-0.5, 0), (-0.5, 0.8), (0,0.8), (0.5, 0.8), (0.5, 0), (0,0)] # Approximated circle
-# delta = 4.0/3.0 * math.tan(math.pi / 8)
-# L = [(0, -1), (-delta, -1), (-1, -delta), (-1, 0), (-1, delta), (-delta, 1), (0, 1), (delta, 1), (1, delta), (1, 0), (1, -delta), (delta, -1), (0, -1)] # Another approximated circle (needs delta)
-
-n = len(L)
-N = 500
-T = np.linspace(0, 1, N, endpoint=True)
-debug = True # set to True to see control points
 
 ##########
 # Script #
@@ -51,7 +30,7 @@ def bezier(L, N):
     return points
 
 # Plot result
-def main():
+def simple_draw():
     p = bezier(L, N)
 
     fig, ax = plt.subplots()
@@ -69,5 +48,64 @@ def main():
 
     plt.show()
 
+def complex_draw():
+    global n
+    fig, ax = plt.subplots()
+    for l in L:
+        n = len(l)
+        p = bezier(l, N)
+        sn = len(l)
+        if debug:
+            l_lb = [[l[i], l[i+1]] for i in range(sn - 1)]
+            lb = mc.LineCollection(l_lb, colors="black", linewidths = 1)
+            ax.add_collection(lb)
+
+        l_cb = [[p[i], p[i+1]] for i in range(N - 1)]
+        
+        cb = mc.LineCollection(l_cb, colors="red", linewidths = 2)
+        ax.add_collection(cb)
+
+    ax.autoscale()
+    ax.axis('equal')
+
+    plt.show()
+
+#########
+#Entries#
+#########
+
+L = []
+delta = 4.0/3.0 * math.tan(math.pi / 8) # For circle
+
+# list of points (simple_draw)
+# L = [(0,0), (1,1), (2,0)] # Conic
+# L = [(0,0), (1,10), (3,-10), (4,0)] # Sin-like
+# L = [(1,1), (0.5,0), (0,0), (0,1), (0.5,1), (1,0)] # Alpha
+# L = [(-0.5,1), (1,0.5), (0,0), (-1,0.5), (0.5,1)] # Gamma
+# L = [(0,2), (1,3), (2,2), (0,0)] # Half heart
+# L = [(0, 2), (1, 2), (1, 0), (1, 0), (0, 0), (-1, 0), (-1, 0), (-1, 2), (0, 2)] # Egg
+# L = [(0,0), (-0.3, 0), (-0.3, 0.3)] # Quarter of circle
+# L = [(0,0), (-0.5, 0), (-0.5, 0.8), (0,0.8), (0.5, 0.8), (0.5, 0), (0,0)] # Approximated circle
+# L = [(0, -1), (-delta, -1), (-1, -delta), (-1, 0), (-1, delta), (-delta, 1), (0, 1), (delta, 1), (1, delta), (1, 0), (1, -delta), (delta, -1), (0, -1)] # Another approximated circle (needs delta)
+
+# list of lists of points (complex_draw)
+# L = [[(0,0), (1,1), (2,0)]] # Conic
+# L = [[(0,2), (1,3), (2,2), (0,0)], [(0,0), (-2,2), (-1,3), (0,2)]] # Heart
+# L = [ [(0,0), (0, 0.3), (-0.3, 0.3)], [(-0.3, 0.3), (0, 0.3), (0, 0.6)], [(0,0.6), (0, 0.3), (0.3, 0.3)], [(0.3,0.3), (0, 0.3), (0, 0)] ] # Hypocycloid
+# L = [ [(0,0), (-0.3, 0), (-0.3, 0.3)], [(-0.3, 0.3), (-0.3, 0.6), (0, 0.6)], [(0,0.6), (0.3, 0.6), (0.3, 0.3)], [(0.3,0.3), (0.3, 0), (0, 0)] ] # Approximated circle, 4 quarters
+# L = [ [(0, -1), (-delta, -1), (-1, -delta), (-1, 0)], [(-1, 0), (-1, delta), (-delta, 1), (0, 1)], [(0, 1), (delta, 1), (1, delta), (1, 0)], [(1, 0), (1, -delta), (delta, -1), (0, -1)] ] # Circle (needs delta)
+
+n = len(L)
+N = 500
+T = np.linspace(0, 1, N, endpoint=True)
+debug = True # set to True to see control points
+
+###########
+#Execution#
+###########
+
 if __name__ == "__main__":
-    main()
+    if type(L[0]) == tuple:
+        simple_draw()
+    else:
+        complex_draw()
